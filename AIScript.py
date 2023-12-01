483f2c99ca2fdd4c2a194fb1d120b8df48b31611
Juan-Altamirano <101400097+Juan-Altamirano@users.noreply.github.com>
2023-11-30 23:37:53 -0300

    Updated AIScript.py
    Still have to translate data, but resizing of input images is underway

--- AIScript.py
+++ AIScript.py
@@ -8,16 +8,48 @@ model = project.version(2).model
 p=0
 cap = cv2.VideoCapture(0)
 
+def image_resize(image, width = None, height = None, inter = cv2.INTER_AREA):
+    # initialize the dimensions of the image to be resized and
+    # grab the image size
+    dim = None
+    (h, w) = image.shape[:2]
+
+    # if both the width and height are None, then return the
+    # original image
+    if width is None and height is None:
+        return image
+
+    # check to see if the width is None
+    if width is None:
+        # calculate the ratio of the height and construct the
+        # dimensions
+        r = height / float(h)
+        dim = (int(w * r), height)
+
+    # otherwise, the height is None
+    else:
+        # calculate the ratio of the width and construct the
+        # dimensions
+        r = width / float(w)
+        dim = (width, int(h * r))
+
+    # resize the image
+    resized = cv2.resize(image, dim, interpolation = inter)
+
+    # return the resized image
+    return resized
+
+
 while True:
     ret, frame = cap.read()
     if ret == False: 
-        print("No hay cámara probablemente")
+        print("Enchufá la cámara capo, o quizás la está usando otra aplicación (como cámara)")
         break
 
     ImgNewName = "Prediction number "+str(p+1)+".jpg"
     p += 1
 
-    IAInput = cv2.imwrite('./images/Imagen_Nro_'+str(p)+'.png',frame) # Esto guarda el frame guardado en la variable frame en la carpeta images, para luego ser usado por la IA
+    IAInput = cv2.imwrite('./images/Imagen_Nro_'+str(p)+'.png',frame) # Esto guarda el frame guardado en la variable frame en la carpeta images como un archivo .jpg, para luego ser usado por la IA
 
     InputRoute = './images/Imagen_Nro_'+str(p)+'.png' # Esto nomás es la ruta para la img, pq no me dejaba ponerla directamente en el model.predict, pq lo toma como más de 1 parámetro
 
@@ -28,6 +60,8 @@ while True:
 
     print(prediction)
 
+    cv2.imshow('IAImgInput', frame)
+
     for i in range(len(prediction)):
 
         localizacion = {'x': int(prediction[i].get('width')) / 2 + int(prediction[i].get('x')), 'y': int(prediction[i].get('height')) / 2 + int(prediction[i].get('y'))}
@@ -37,9 +71,15 @@ while True:
 
         print (localizacion)
 
-        # cv2.circle(frame, (int(localizacion.get('x')), int(localizacion.get('y'))), 5, (0, 0, 255), -1)
+        cv2.circle(frame, (int(localizacion.get('x')), int(localizacion.get('y'))), 5, (0, 0, 255), -1)
+
+    if cv2.waitKey(1) == ord('q'):
+        break
 
     time.sleep(4)
+
+cv2.destroyAllWindows()
+
 
 # Prioridades = sorted(localizacion.get['x'])
 
